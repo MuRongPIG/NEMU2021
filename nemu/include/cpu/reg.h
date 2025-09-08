407a73313464a5ba803c05c28dfb6d5bdebb1c05
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-08 20:52:03 +0800

    >  compile NEMU 3024244133 simpleedu Linux test-KVM 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:52:03 up  2:20,  1 user,  load average: 0.09, 0.03, 0.01 a01f4d8178ba128e944731aa9fbda86132708383

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -23,10 +23,10 @@ typedef struct {
 		} gpr[8];
 
 		/* Do NOT change the order of the GPRs' definitions. */
-
-		struct {
-			uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
-		};
+		// struct {
+		// 	uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+		// };
+		uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
 	};
 
      swaddr_t eip;
