c3b1c65cbfafc66a7a9c308b4247c2dba505a129
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-09 20:55:10 +0800

    >  compile NEMU 3024244133 simpleedu Linux test-KVM 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:55:10 up 10:01,  1 user,  load average: 0.19, 0.10, 0.02 689541dac47fda8388877670ad3251f72a635327

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -187,6 +187,7 @@ int find_dominant_operator(int p,int q,bool *success) {
 uint32_t get_reg_val(const char *s,bool *success);
 
 uint32_t eval(int p,int q,bool *success) {
+	printf("%d %d\n",p,q);
 	if(p > q) {
 		// 表达式异常
 		// assert(0);
