93724ae12ced51130139a0c4d0cb58f18025f61f
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-09 18:13:13 +0800

    >  compile NEMU 3024244133 simpleedu Linux test-KVM 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:13:13 up  7:20,  1 user,  load average: 0.01, 0.03, 0.07 7b0c4f7358d627304e73480c360c5bb9aa90777c

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -103,7 +103,6 @@ static bool make_token(char *e) {
 						Assert(substr_len < 32, "length of int is too long (> 31)");
 						strncpy(tokens[nr_token].str, substr_start, substr_len);
 						tokens[nr_token].str[substr_len] = '\0';
-						break;
 					default: 
 						tokens[nr_token++].type = rules[i].token_type;
 				}
