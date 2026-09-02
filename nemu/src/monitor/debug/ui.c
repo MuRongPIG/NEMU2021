c734c6b7a5bf354417ed5b8602e15e9a2071ca0f
tracer-NEMU2020 <tjuics2020@163.com>
2026-09-02 03:43:49 +0000

    >  compile NEMU 2020244008 trailblazer Linux 404bdfd907d7 6.12.96+deb13-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.12.96-1 (2026-07-20) x86_64 x86_64 x86_64 GNU/Linux  03:43:49 up 4 days, 20:15,  0 users,  load average: 0.14, 0.10, 0.09 47b07de9a04260141b56c0a1eb8473449e0e5b8

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -36,6 +36,13 @@ static int cmd_q(char *args) {
 	return -1;
 }
 
+static int cmd_si(char *args) {
+    char *arg = strtok(NULL, " ");
+    int steps = (arg != NULL) ? atoi(arg) : 1;
+    if (steps > 0) cpu_exec(steps);
+    return 0;
+}
+
 static int cmd_help(char *args);
 
 static struct {
@@ -46,7 +53,7 @@ static struct {
 	{ "help", "Display informations about all supported commands", cmd_help },
 	{ "c", "Continue the execution of the program", cmd_c },
 	{ "q", "Exit NEMU", cmd_q },
-
+	{ "si", "Step into the next instruction", cmd_si },
 	/* TODO: Add more commands */
 
 };
